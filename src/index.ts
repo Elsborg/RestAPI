657ab22b5fce308374cba5baa4a156bd9432f10c
Martin <martin.elsborg@gmail.com>
2024-01-14 20:49:49 +0100

    mongoose configuration and user schema created

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -22,3 +23,8 @@ server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/')
 })
 
+const MONGO_URL = 'mongodb+srv://martin:Martin@cluster0.dyfow6f.mongodb.net/?retryWrites=true&w=majority'
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error', (error: Error) => console.log(error))
